0b07d2fa444c8409c0e7c8e7594b98df2e9f4b26
Apollo24K <64652448+Apollo24K@users.noreply.github.com>
2023-03-24 16:17:26 +0100

    Delete Camelot directory

--- Camelot/Commands/recommend.js
+++
@@ -1,9 +0,0 @@
-const { auniq } = require("../Modules/chars.js");
-
-module.exports = {
-	name: 'recommend',
-	description: 'recommend an anime',
-	execute(interaction) {
-        return interaction.reply(auniq[Math.floor(Math.random() * auniq.length)]);
-    },
-};
