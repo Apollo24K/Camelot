a49704680ac2b95dcd9039d23d6fb0157ae3fd32
pokelink <stankat2509@gmail.com>
2025-12-23 16:03:47 +0100

    Fix

--- Modules/chars.ts
+++ Modules/chars.ts
@@ -25796,7 +25796,6 @@ export const characters: charInfo[] = [
     new charInfo("Ramashut", [], anime[682], "F", "https://i.ibb.co/1JBTSZ61/c.png", 25692, "C"),
     new charInfo("Volkov Varanov", [], anime[682], "M", "https://i.ibb.co/NnKVDCxT/c.png", 25693, "C"),
     new charInfo("Zuber", [], anime[682], "M", "https://i.ibb.co/yn2v0M3C/c.png", 25694, "C"),
-
     new charInfo("Kajou Ayame", ["Blue Tundra", "The Blue in Snowfield", "Blue Snow", "Kajou"], anime[683], "F", "https://i.ibb.co/whJTRGJf/c.png", 25695, "SS"),
     new charInfo("Nishikinomiya Anna", ["Anna-senpai", "Chatty Cathy", "Sweet Angel"], anime[683], "F", "https://i.ibb.co/tTTMrhn3/c.png", 25696, "S"),
     new charInfo("Okuma Tanukichi", ["Sentimental Bomber", "Mister Baiter", "Meister Baiter", "Mr. Okuma", "Brother", "Big Brother (Shimoneta)", "Big Bro", "Brother Slave"], anime[683], "M", "https://i.ibb.co/WNRZp0cM/c.png", 25697, "A"),
